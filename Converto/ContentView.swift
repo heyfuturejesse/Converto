c7eb086ed5813bbf791a2026f033160877434603
heyfuturejesse <jesse@heyfuturejesse.com>
2020-06-22 12:43:10 +1000

    Initial Commit

---
+++ Converto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converto
+//
+//  Created by Jesse Russell on 22/6/20.
+//  Copyright © 2020 Jesse Russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
